c5db33834026817b2d8798338139064bb28140cd
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-01 12:30:13 +0200

    CAr class Update

--- src/day38_Inheritance/CarTask/Tesla.java
+++ src/day38_Inheritance/CarTask/Tesla.java
@@ -4,6 +4,7 @@ public class Tesla extends Car{
     public Tesla(String model, String colour, int year, double price, double miles) {
         super("Tesla", model, colour, year, price, miles);
     }
+
     public void autoPilot(){
         System.out.println(brand+" "+model+" in auto pilot mode");
     }
