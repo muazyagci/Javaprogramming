15278433f42d4aa9525b5d016b02a3c0b53ec565
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-01 09:30:54 +0200

    Car Task initial

---
+++ src/day38_Inheritance/Car.java
@@ -0,0 +1,34 @@
+package day38_Inheritance;
+
+public class Car {
+    public String brand, model, colour;
+    public int year;
+    public double price,miles;
+
+    public Car(String brand, String model, String colour, int year, double price, double miles) {
+        this.brand = brand;
+        this.model = model;
+        this.colour = colour;
+        this.year = year;
+        this.price = price;
+        this.miles = miles;
+    }
+
+    public void start(){
+        System.out.println(brand+" "+model+" is started");
+    }
+    public void drive(){
+        System.out.println(brand+" "+model+" is driving");
+    }
+
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", colour='" + colour + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", miles=" + miles +
+                '}';
+    }
+}
