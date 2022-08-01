44232191a85715e3300965f7d0d152561e5533f8
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-01 12:39:14 +0200

    ParkingLot class created

---
+++ src/day38_Inheritance/CarTask/ParkingLot.java
@@ -0,0 +1,16 @@
+package day38_Inheritance.CarTask;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+        Toyota toyota=new Toyota("Camry","Green",2020,20000,15400);
+        System.out.println(toyota);
+
+        Tesla tesla=new Tesla("ModelS","White",2022,57000,65000);
+        System.out.println(tesla);
+
+        BMW bmw=new BMW("116","White",2019,19000,26000);
+
+
+
+    }
+}
