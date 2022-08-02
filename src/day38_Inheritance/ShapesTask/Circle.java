a76165a03ac9a9dbeb1fd8eb6ded895273fa73a8
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-02 09:41:42 +0200

    Shape class created

---
+++ src/day38_Inheritance/ShapesTask/Circle.java
@@ -0,0 +1,19 @@
+package day38_Inheritance.ShapesTask;
+
+public class Circle extends Shape{
+//    public double
+
+    public Circle(String name) {
+        super(name);
+    }
+//
+//    @Override
+//    public double area() {
+//        return ;
+//    }
+//
+//    @Override
+//    public double perimeter() {
+//        return ;
+//    }
+}
