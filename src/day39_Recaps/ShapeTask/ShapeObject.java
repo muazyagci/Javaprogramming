a3a0fbba93c71bdb5f4bad12a0810853da293385
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-02 13:10:16 +0200

    day 39 Shape object created

---
+++ src/day39_Recaps/ShapeTask/ShapeObject.java
@@ -0,0 +1,15 @@
+package day39_Recaps.ShapeTask;
+
+public class ShapeObject {
+    public static void main(String[] args) {
+
+    Square square=new Square(5);
+        System.out.println(square);
+
+        Rectangle rectangle=new Rectangle(10,6);
+        System.out.println(rectangle);
+
+        Circle circle=new Circle(9);
+        System.out.println(circle);
+    }
+}
