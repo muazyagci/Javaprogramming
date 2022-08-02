0afd07a23d95b876c7741c0a94030bec5531285a
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-02 12:59:19 +0200

    day 39 Shape task update

---
+++ src/day39_Recaps/ShapeTask/Circle.java
@@ -0,0 +1,33 @@
+package day39_Recaps.ShapeTask;
+
+public class Circle extends Shape{
+
+    private double radius;
+    public static double pi;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if (radius<=0){
+            System.err.println("Invalid radius");
+            System.exit(1);
+        }
+
+        this.radius = radius;
+    }
+
+
+
+    public Circle(String name, double radius) {
+        super(name);
+        setRadius(radius);
+
+    }
+
+    static {
+pi=3.14;
+    }
+
+}
--- src/day39_Recaps/ShapeTask/Rectangle.java
+++ src/day39_Recaps/ShapeTask/Rectangle.java
@@ -31,9 +31,28 @@ private double length,width;
     }
 
     public Rectangle(String name, double length, double width) {
-        super(name);
+        super("Rectangle");
         setLength(length);
         setWidth(width);
+    }
+
+    @Override
+    public double area() {
+        return length*width;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2*(length+width);
+    }
+
+    public String toString() {
+        return "Rectangle{" +
+                "length=" + length +
+                ", width=" + width +
+                ", area=" + area() +
+                ", perimeter=" + perimeter() +
+                '}';
     }
 
 }
