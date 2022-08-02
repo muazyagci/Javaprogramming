5c2bf91e73c5aa21d4e013481eff0511b0c9bea7
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-02 18:44:15 +0200

    CydeoTask update

--- src/day39_Recaps/CydeoTask/Developer.java
+++ src/day39_Recaps/CydeoTask/Developer.java
@@ -1,9 +1,13 @@
 package day39_Recaps.CydeoTask;
 
-public class Developer {
+public class Developer extends Employee{
+
+    public Developer(String name, char gender, int age, long ID, String jobTitle, double salary) {
+        super(name, gender, age, ID, jobTitle, salary);
+    }
 
     public void fixingBugs(){
-
+        System.out.println(getJobTitle()+" "+getName()+" is fixing the bug");
     }
 
 }
