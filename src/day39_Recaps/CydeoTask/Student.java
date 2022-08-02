6479d8004967c979af13f15753bf363445ac7fbf
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-02 18:56:57 +0200

    CydeoTask update

--- src/day39_Recaps/CydeoTask/Student.java
+++ src/day39_Recaps/CydeoTask/Student.java
@@ -1,4 +1,33 @@
 package day39_Recaps.CydeoTask;
 
-public class Student {
+public class Student extends Person {
+    private int studentID;
+    private String fieldOfStudy;
+
+    public Student(String name, char gender, int age, int studentID, String fieldOfStudy) {
+        super(name, gender, age);
+        setStudentID(studentID);
+        setFieldOfStudy(fieldOfStudy);
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getFieldOfStudy() {
+        return fieldOfStudy;
+    }
+
+    public void setFieldOfStudy(String fieldOfStudy) {
+        this.fieldOfStudy = fieldOfStudy;
+    }
+    public void study(){
+        System.out.println(getName()+" is studying");
+    }
+
 }
+
