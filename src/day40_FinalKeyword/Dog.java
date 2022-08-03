951f509c53f007543d1a3309f8d0ff1cfc9a3310
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-03 18:13:44 +0200

    Final class created

--- src/day40_FinalKeyword/Dog.java
+++ src/day40_FinalKeyword/Dog.java
@@ -6,4 +6,15 @@ public class Dog extends Animal{
         super(name, breed, gender, colour, size, age);
     }
 
+    @Override
+    public void eat() {
+        System.out.println(getName()+" is eating dog food");
+    }
+
+//    public void drink(){// final methods can not be overridden
+//        System.out.println(getName()+" is drinking beer");
+//    }
+
+
+
 }
---
+++ src/day40_FinalKeyword/Zoo.java
@@ -0,0 +1,9 @@
+package day40_FinalKeyword;
+
+public class Zoo {
+    public static void main(String[] args) {
+
+        Dog dog=new Dog("Mahmut","Kangal",'M',"White","medium",5);
+        System.out.println();
+    }
+}
