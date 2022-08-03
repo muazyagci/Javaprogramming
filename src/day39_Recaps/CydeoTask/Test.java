cf58f5264d0cf067487ea5132c6062ec8dce2392
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-03 12:35:52 +0200

    CydeoTask update

---
+++ src/day39_Recaps/CydeoTask/Test.java
@@ -0,0 +1,16 @@
+package day39_Recaps.CydeoTask;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Developer developer=new Developer("Yunus",'F',32,98775,"java developer",5500);
+
+        System.out.println(developer);
+
+//        for (int i = 0; i <5 ; i++) {
+//            System.out.println(developer.getName()+" "+developer.getSalary());
+//        }
+
+    }
+
+}
