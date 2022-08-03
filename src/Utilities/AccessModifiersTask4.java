78b410fcae6ee4edb79ae060125c2c5a975b60ce
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-03 13:58:22 +0200

    Final class created

---
+++ src/Utilities/AccessModifiersTask4.java
@@ -0,0 +1,7 @@
+package Utilities;
+
+public class AccessModifiersTask4 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/day40_FinalKeyword/ProtectedAccessModifier.java
@@ -0,0 +1,8 @@
+package day40_FinalKeyword;
+
+public class ProtectedAccessModifier {
+    static String name1 = "Cydeo";
+    protected static String name2 = "WoodenSpoon";
+
+
+}
