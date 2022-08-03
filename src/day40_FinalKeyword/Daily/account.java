1b2343e2320a8e599f5a4b2f6a8244ef146d50cb
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-03 22:51:47 +0200

    Final class created

--- src/day40_FinalKeyword/Daily/account.java
+++ src/day40_FinalKeyword/Daily/account.java
@@ -2,20 +2,18 @@ package day40_FinalKeyword.Daily;
 
 public class account {
     public String userName,passWord;
+static String correctUserName="ozgurguler",CorrectPassword="anadoluatesi";
 
-    public account(String userName, String passWord) {
-        this.userName = userName;
-        this.passWord = passWord;
-    }
 
     public void login() {
         boolean a = true;
         while (a) {
-            if (userName.equals("ozgurguler") && passWord.equals("anadoluatesi")) {
+            if (userName.equals(correctUserName) && passWord.equals(CorrectPassword)) {
                 System.out.println("Logged In");
                 a=false;
             }else {
                 System.err.println("Wrong password.Try again");
+                break;
             }
 
         }
--- src/day40_FinalKeyword/Daily/test.java
+++ src/day40_FinalKeyword/Daily/test.java
@@ -1,11 +1,34 @@
 package day40_FinalKeyword.Daily;
 
-public class test {
+import java.util.Scanner ;
+
+public class test extends account {
+
     public static void main(String[] args) {
+Scanner scan=new Scanner(System.in);
+        System.out.println("Please enter userName and passWord");;
+account account=new account();
+        System.out.println("Enter username: ");
+        account.userName= scan.next();
+        System.out.println("Enter password: ");
+        account.passWord= scan.next();
+boolean a=true;
+while (a){
+    if (account.userName.equals(correctUserName)&&account.equals(CorrectPassword)){
+        System.out.println("Logged in");
+        break;
+    }else {
+        System.err.println("Invalid");
+        System.err.println("Please enter again");
 
-        account account=new account("ozgurguler","anadoluatesi");
-        account.login();
+        account.userName= scan.next();
+        account.passWord= scan.next();
 
+    }
+
+}
+
+account.login();
 
     }
 }
