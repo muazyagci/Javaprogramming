217b31658ca73818ccf6977f005ca980f7135725
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-03 17:19:53 +0200

    Final class created

---
+++ src/day40_FinalKeyword/FinalVariable.java
@@ -0,0 +1,19 @@
+package day40_FinalKeyword;
+
+public class FinalVariable {
+    public static void main(String[] args) {
+       final double pi=3.14;
+
+//        pi=1.14; final variables can not be changed
+//        pi =5.14;
+
+
+        final String name;
+        name="Java";
+
+       // name="Wooden Spoon";
+
+        System.out.println(name);
+
+    }
+}
