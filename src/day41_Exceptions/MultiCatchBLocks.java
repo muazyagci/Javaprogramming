df46637c7fa6fb06e351accfbd9aba4408d7a645
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-20 00:38:34 +0200

    Exceptions

--- src/day41_Exceptions/MultiCatchBLocks.java
+++ src/day41_Exceptions/MultiCatchBLocks.java
@@ -5,28 +5,34 @@ import day39_Recaps.CydeoTask.Employee;
 public class MultiCatchBLocks {
     public static void main(String[] args) {
 
-        Employee employee=null;
+        Employee employee = null;
 
 
-
-        try{
+        try {
             System.out.println(employee.getSalary());
-        }catch (IndexOutOfBoundsException e){
+        } catch (IndexOutOfBoundsException e) {
             System.out.println("First catch block");
             e.printStackTrace();
-        }catch (ArithmeticException e){
+        } catch (ArithmeticException e) {
             System.out.println("Second catch block");
             e.printStackTrace();
-        }catch (NullPointerException e){
+        } catch (NullPointerException e) {
             System.out.println("Third catch block");
             e.printStackTrace();
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             System.out.println("Fourth Block");
-        e.printStackTrace();
+            e.printStackTrace();
         }
         System.out.println("Test complete");
 
-
+        System.out.println("------------------------");
+        try {
+            System.out.println("Java".charAt(-1));
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            System.out.println("cought");
+        }
+        System.out.println("Done");
 
 
     }
