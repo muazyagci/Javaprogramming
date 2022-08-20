b73efa539f4db4c6bbccece184f63da143af415f
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-20 19:54:39 +0200

    Exceptions

--- src/day42_Exceptions/ThrowsKeyword2.java
+++ src/day42_Exceptions/ThrowsKeyword2.java
@@ -1,11 +1,17 @@
 package day42_Exceptions;
 
 public class ThrowsKeyword2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ArithmeticException{
         System.out.println("Test started");
 
-        System.out.println(8/0);
 
+try{
+    System.out.println(8/0);
+}catch (ArithmeticException e){
+    e.printStackTrace();
+
+}
         System.out.println("Test Complete");
+
     }
 }
