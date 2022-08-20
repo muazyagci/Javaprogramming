300bb88ed201856f3868eaa19c6922335c849439
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-20 20:09:55 +0200

    Exceptions

--- src/day42_Exceptions/DisadvantageOfThrowsKeyword2.java
+++ src/day42_Exceptions/DisadvantageOfThrowsKeyword2.java
@@ -11,5 +11,16 @@ public class DisadvantageOfThrowsKeyword2 {
     public static void method2()throws FileNotFoundException{
         method1();
     }
+public static void method3() throws FileNotFoundException, InterruptedException {
+        method1();
+        method2();
+        Thread.sleep(1500);
+
+}
+public static void method4() throws FileNotFoundException, InterruptedException {
+        method1();
+        method2();
+        method3();
+}
 
 }
