ddf17a56bb67b03bb0e98302f26368739c3ed618
Muhammet Muaz <m.muazyagci@gmail.com>
2022-08-20 15:08:35 +0200

    Exceptions

--- src/day42_Exceptions/ThrowsKeyword.java
+++ src/day42_Exceptions/ThrowsKeyword.java
@@ -1,7 +1,29 @@
 package day42_Exceptions;
 
 public class ThrowsKeyword {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("-----------Test-----------");
+
+        System.out.println("Hello");
+        Thread.sleep(3000);
+
+        System.out.println("Cydeo");
+
+
+        System.out.println("--------------- Test 1 Complete ------------------");
+
+        System.out.println("--------------- Test 2----------------------------");
+
+        System.out.println("Hello");
+        Thread.sleep(3000);
+        System.out.println("Batch 25");
+
+        System.out.println("------------------Test 2 Complete -----------------");
+
+
+
+
 
     }
 }
