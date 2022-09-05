2c06df01af11c2f8d6f8f088c5088f1eeab13565
Muhammet Muaz <m.muazyagci@gmail.com>
2022-09-06 00:37:28 +0200

    Abstraction-Interface

---
+++ src/Replit/class1.java
@@ -0,0 +1,13 @@
+package Replit;
+
+public class class1 {
+    public static void main(String[] args) {
+//        boolean yunusIsgay=true;
+//        while (yunusIsgay){
+//            System.out.println("YUNUS IS NIQQA AND GAY");
+//        }
+
+
+
+    }
+}
