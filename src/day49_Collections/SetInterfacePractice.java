a2ea7204db6b60413b69a60a0a291edd67ea35b6
Muhammet Muaz <m.muazyagci@gmail.com>
2022-09-13 09:55:13 +0200

    Collections

--- src/day49_Collections/SetInterfacePractice.java
+++ src/day49_Collections/SetInterfacePractice.java
@@ -1,7 +1,36 @@
 package day49_Collections;
 
+import java.util.*;
+
 public class SetInterfacePractice {
     public static void main(String[] args) {
+
+        List<Integer> list=new ArrayList<>();
+        list.addAll(Arrays.asList(10,200,300,40,90));
+        list.addAll(Arrays.asList(10,200,300,40,90));
+        list.addAll(Arrays.asList(10,200,300,40,90));
+        list.addAll(Arrays.asList(10,200,300,40,90));
+        list.addAll(Arrays.asList(10,200,300,40,90));
+
+        System.out.println(list);
+        System.out.println(list.get(4));
+        System.out.println("------------------------------");
+        Set<Integer> hashSet1=new HashSet<>();
+        hashSet1.addAll(Arrays.asList(10,200,300,40,90));
+        hashSet1.addAll(Arrays.asList(10,200,300,40,90));
+        hashSet1.addAll(Arrays.asList(10,200,300,40,90));
+        hashSet1.addAll(Arrays.asList(10,200,300,40,90));
+        hashSet1.addAll(Arrays.asList(10,200,300,40,90));
+        hashSet1.addAll(Arrays.asList(null,null,null,null));
+
+
+
+        System.out.println(hashSet1);
+
+
+        Set<Integer> linkedHashSet1=new LinkedHashSet<>();
+
+        Set<Integer> treeSet1=new TreeSet<>();
 
     }
 }
