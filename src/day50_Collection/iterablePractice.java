d5ea6cdf38a98c65aebdf8ce8be6fa17f038dd3c
Muhammet Muaz <m.muazyagci@gmail.com>
2022-09-14 15:38:54 +0200

    Collections

--- src/day50_Collection/iterablePractice.java
+++ src/day50_Collection/iterablePractice.java
@@ -2,6 +2,7 @@ package day50_Collection;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class iterablePractice {
@@ -13,7 +14,52 @@ public class iterablePractice {
                 list.remove(i);
             }
 
+
         }
+
+        System.out.println("list = " + list);
+        System.out.println("==========================================");
+
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(1,2,3,4,5,1,2,3,4,5,1,2,3,4,5));
+
+        Iterator<Integer> it = list2.iterator();
+/*
+        boolean r1= it.hasNext();
+
+        System.out.println(r1);
+
+        System.out.println(list2);
+
+
+ */
+
+
+        while (it.hasNext()){
+            if (it.next()<4){
+                it.remove();
+            }
+        }
+
+        System.out.println(list2);
+        System.out.println("==========================================");
+
+        List<Integer> list3 = new ArrayList<>(Arrays.asList(1,2,3,4,5,1,2,3,4,5,1,2,3,4,5));
+
+        for (Iterator<Integer>i=list3.iterator();i.hasNext();){
+            if (i.next()<4){
+                i.remove();
+            }
+
+        }
+
+        System.out.println(list3);
+
+        System.out.println("==========================================");
+
+        List<Integer> list4 = new ArrayList<>(Arrays.asList(1,2,3,4,5,1,2,3,4,5,1,2,3,4,5));
+
+        list4.removeIf(each -> each<4 );
+        System.out.println(list4);
 
     }
 }
