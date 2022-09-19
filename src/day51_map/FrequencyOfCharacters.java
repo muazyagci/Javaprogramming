47e1f305a63e12825a8d5256f33aa633e0892093
Muhammet Muaz <m.muazyagci@gmail.com>
2022-09-19 19:09:11 +0200

    Collections

--- src/day51_map/FrequencyOfCharacters.java
+++ src/day51_map/FrequencyOfCharacters.java
@@ -6,7 +6,15 @@ public class FrequencyOfCharacters {
     public static void main(String[] args) {
 
         String str="bbvvvaaaaa";
+        String[] arr=str.split("");
         Map<String, Integer> frequency=new LinkedHashMap<>();
+
+        for (String each : arr) {
+            int frequency2=Collections.frequency(Arrays.asList(arr),each);
+            frequency.put(each,frequency2);
+        }
+
+        System.out.println(frequency);
 
     }
 }
