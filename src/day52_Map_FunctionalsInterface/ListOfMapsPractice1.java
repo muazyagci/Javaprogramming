92bde798f981d77065b26b040fff3ab8e8f84967
Muhammet Muaz <m.muazyagci@gmail.com>
2022-09-20 15:09:53 +0200

    Collections

--- src/day52_Map_FunctionalsInterface/ListOfMapsPractice1.java
+++ src/day52_Map_FunctionalsInterface/ListOfMapsPractice1.java
@@ -5,18 +5,18 @@ import java.util.*;
 public class ListOfMapsPractice1 {
     public static void main(String[] args) {
         Map<String, String> scrumTeam1 = new LinkedHashMap<>();
-        scrumTeam1.put("Abdulhamid","SM");
-        scrumTeam1.put("Nikita","Developer");
-        scrumTeam1.put("Alina","Developer");
-        scrumTeam1.put("Mert","PO");
-        scrumTeam1.put("Lee","SDET");
+        scrumTeam1.put("Abdulhamid", "SM");
+        scrumTeam1.put("Nikita", "Developer");
+        scrumTeam1.put("Alina", "Developer");
+        scrumTeam1.put("Mert", "PO");
+        scrumTeam1.put("Lee", "SDET");
         Map<String, String> scrumTeam2 = new LinkedHashMap<>();
-        scrumTeam2.put("Anton","SDET");
-        scrumTeam2.put("Muhammed","Developer");
-        scrumTeam2.put("Gulistan","Developer");
-        scrumTeam2.put("Ahmad","PO");
-        scrumTeam2.put("Nevim","SM");
-        scrumTeam2.put("Atakan","SDET");
+        scrumTeam2.put("Anton", "SDET");
+        scrumTeam2.put("Muhammed", "Developer");
+        scrumTeam2.put("Gulistan", "Developer");
+        scrumTeam2.put("Ahmad", "PO");
+        scrumTeam2.put("Nevim", "SM");
+        scrumTeam2.put("Atakan", "SDET");
         Map<String, String> scrumTeam3 = new LinkedHashMap<>();
         scrumTeam3.put("YanChun", "BA");
         scrumTeam3.put("Tahir", "Developer");
@@ -27,27 +27,59 @@ public class ListOfMapsPractice1 {
         Map<String, String> scrumTeam4 = new LinkedHashMap<>();
         scrumTeam4.put("Alim", "PO");
         scrumTeam4.put("Kaiser", "SDET");
-        scrumTeam4.put("Alinur","SDET");
-        scrumTeam4.put("Farya","Develepor");
+        scrumTeam4.put("Alinur", "SDET");
+        scrumTeam4.put("Farya", "Develepor");
         scrumTeam4.put("Sherzod", "Developer");
         scrumTeam4.put("Gulsen", "SM");
-        scrumTeam4.put("David","Sdet");
+        scrumTeam4.put("David", "Sdet");
         Map<String, String> scrumTeam5 = new LinkedHashMap<>();
-        scrumTeam5.put("Elza","SDET");
-        scrumTeam5.put("Mehmet","Developer");
-        scrumTeam5.put("Stefan","Developer");
-        scrumTeam5.put("Rishat","PO");
-        scrumTeam5.put("Ismail","SM");
-        scrumTeam5.put("Tamara","SDET");
-        scrumTeam5.put("Adnan","QA");
+        scrumTeam5.put("Elza", "SDET");
+        scrumTeam5.put("Mehmet", "Developer");
+        scrumTeam5.put("Stefan", "Developer");
+        scrumTeam5.put("Rishat", "PO");
+        scrumTeam5.put("Ismail", "SM");
+        scrumTeam5.put("Tamara", "SDET");
+        scrumTeam5.put("Adnan", "QA");
 
-        List<Map<String,String>> scrumTeams=new ArrayList<>();
+        List<Map<String, String>> scrumTeams = new ArrayList<>();
 
         scrumTeams.add(scrumTeam1);
-       scrumTeams.addAll(Arrays.asList(scrumTeam2,scrumTeam3,scrumTeam4,scrumTeam5));
+        scrumTeams.addAll(Arrays.asList(scrumTeam2, scrumTeam3, scrumTeam4, scrumTeam5));
 
-        System.out.println(scrumTeams);
-        System.out.println(scrumTeams.size());
+//        System.out.println(scrumTeams);
+//        System.out.println(scrumTeams.size());
+
+        for (Map<String, String> eachScrumTeam : scrumTeams) {
+            for (Map.Entry<String, String> each : eachScrumTeam.entrySet()) {
+                System.out.println(each.getKey() + " : " + each.getValue());
+            }
+        }
+        System.out.println("*-------------------------------------*");
+/*
+        for (Map<String, String> each : scrumTeams) {
+            for (String each2 : each.keySet()) {
+                if (each.get(each2).equals("SM")) {
+                    System.out.println(each2);
+                }
+            }
+        }
+
+ */
+
+
+        for (Map<String, String> each : scrumTeams) {
+            for (Map.Entry<String, String> each2 : each.entrySet()) {
+                if (each2.getValue().equals("SM")){
+                    System.out.println(each2.getKey());
+                }
+                
+            }
+        }
+
+
+
+
+
 
     }
 }
