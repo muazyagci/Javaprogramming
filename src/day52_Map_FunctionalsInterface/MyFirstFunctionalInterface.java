ca29ea17d25caac39876d736c7fccb0153ad6e1a
Muhammet Muaz <m.muazyagci@gmail.com>
2022-09-20 20:08:45 +0200

    Collections

--- src/day52_Map_FunctionalsInterface/MyFirstFunctionalInterface.java
+++ src/day52_Map_FunctionalsInterface/MyFirstFunctionalInterface.java
@@ -1,6 +1,9 @@
 package day52_Map_FunctionalsInterface;
 
+@FunctionalInterface
 public interface MyFirstFunctionalInterface {
+
+        void apply(int n);
 
 
 
---
+++ src/day52_Map_FunctionalsInterface/Test.java
@@ -0,0 +1,11 @@
+package day52_Map_FunctionalsInterface;
+
+public class Test {
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+}
